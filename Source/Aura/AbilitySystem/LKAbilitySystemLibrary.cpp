6d3980bc8e8bee00cd0fff91bc64c45e48691c6a
Gaston <gastonsimonetti85@gmail.com>
2025-02-27 08:53:02 -0300

    Section 17 done

--- Source/Aura/AbilitySystem/LKAbilitySystemLibrary.cpp
+++ Source/Aura/AbilitySystem/LKAbilitySystemLibrary.cpp
@@ -159,3 +159,11 @@ void ULKAbilitySystemLibrary::GetLivePlayersWithinRadius(const UObject* WorldCon
 		}
 	}
 }
+
+bool ULKAbilitySystemLibrary::IsNotFriend(AActor* FirstActor, AActor* SecondActor)
+{
+	const bool bBothArePlayers = FirstActor->ActorHasTag(FName("Player")) && SecondActor->ActorHasTag(FName("Player"));
+	const bool bBothAreEnemies = FirstActor->ActorHasTag(FName("Enemy")) && SecondActor->ActorHasTag(FName("Enemy"));
+	const bool bFriends = bBothArePlayers || bBothAreEnemies;
+	return !bFriends;
+}
